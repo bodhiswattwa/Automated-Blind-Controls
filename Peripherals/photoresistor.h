6a3b1ec9fcd3ec10a34920061c118c24d7fe830a
Bodhiswattwa Basu <66216074+bodhiswa@users.noreply.github.com>
2022-04-14 19:20:22 -0400

    Update photoresistor.h

--- Peripherals/photoresistor.h
+++ Peripherals/photoresistor.h
@@ -1,3 +1,14 @@
+// File Name: photoresistor.h
+// File Last Updated: 04/14/2022
+// Authors: Gabriel Yengle, Avi Tombak, Jesse Both and Bodhiswattwa Basu
+// File Description: The following file is the file required to read from the photoresistor and get the intensity of light readings.
+// File Functions and Returned Values:
+//      1. check_photo_resistor() -> intensity of light.
+// File Inclusions: 
+//      1. stdio.h
+//      2. mbed.h
+//      3. string.h
+
 #include "mbed.h"
 #include <stdio.h>
 #include <string.h>
