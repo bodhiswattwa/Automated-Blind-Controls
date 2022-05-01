8cf6582ab54869df7c9a742a5c52938bfd22c578
jesseboth <jessebot@buffalo.edu>
2022-05-01 17:15:01 -0400

    testing stepper

--- Peripherals/main.cpp
+++ Peripherals/main.cpp
@@ -4,10 +4,12 @@
 #include "DHT.h"
 #include "PinNames.h"
 
+#include "stepper_test.h"
+
 #define DHTC PA_3
 #define LIGHT PA_4      // pin for light sensor
 
-DHT::DHT(PA_3);
+DHT11 dht(PA_3);
 AnalogIn light_sens(LIGHT); 
 
 
@@ -16,8 +18,11 @@ int main()
 {
     int val = 0;
     int intensity = 0;
+    step_test(0, 0);
     while (true) {
-        printf("%f\n", DHT::ReadTemperature());
+        printf("here\n");
+        fullstep_anticlockwise();
+        // printf("%f\n", dht.getFahrenheit());
         //printf("%d\n", val);
     }
 }
