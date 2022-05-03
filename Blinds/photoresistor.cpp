2617abe88b2efb4285f9a7b4697054a8e089f173
jesseboth <jessebot@buffalo.edu>
2022-05-03 12:41:00 -0400

    Build fix

--- Blinds/photoresistor.cpp
+++ Blinds/photoresistor.cpp
@@ -12,28 +12,31 @@
 #include "mbed.h"
 #include <stdio.h>
 #include <string.h>
+#include "photoresistor.h"
+
+AnalogIn light_sens(LIGHT); 
 
 int check_photo_resistor()
 {
     int val = 0;
     int intensity = 0;
-    while (true) {
-        val = light_sens.read_u16();    
-        if(val > 50000){
-            intensity = 5;
-        } 
-        else if (val > 40000 ){
-            intensity = 4;
-        } 
-        else if(val > 30000){
-            intensity = 3;
-        } 
-        else if(val > 20000){
-            intensity = 2;
-        } else {
-           intensity = 1;
-        }
+
+    val = light_sens.read_u16();    
+    if(val > 50000){
+        intensity = 5;
+    } 
+    else if (val > 40000 ){
+        intensity = 4;
+    } 
+    else if(val > 30000){
+        intensity = 3;
+    } 
+    else if(val > 20000){
+        intensity = 2;
+    } else {
+        intensity = 1;
     }
+
     return intensity;
 }
 
--- Blinds/photoresistor.h
+++ Blinds/photoresistor.h
@@ -1,5 +1,3 @@
-
 #define LIGHT PA_4      // pin for light sensor
-AnalogIn light_sens(LIGHT); 
 
 int check_photo_resistor();
